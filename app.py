6eeb0eef530a7b22fd9cfca6bdfed145a435e492
mailliwJ <167079494+mailliwJ@users.noreply.github.com>
2024-12-10 17:21:16 +0100

    df changed to tables

--- app.py
+++ app.py
@@ -107,11 +107,11 @@ def retrain():
             left_table, right_table = st.columns(2)
             with left_table:
                 st.markdown('Current Model Metrics')
-                st.dataframe(st.session_state['current_metrics'])
+                st.table(st.session_state['current_metrics'])
                     
             with right_table:
                 st.markdown('Updated Dataset Metrics')
-                st.dataframe(st.session_state['new_metrics'])
+                st.table(st.session_state['new_metrics'])
 
             if st.button('Save Dataset and Retrained Model'):
                 with st.spinner('Saving data and retraining model...'):
